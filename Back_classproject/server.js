a5efdd80a47cb32f77b7273ec69c4dfe992dbeeb
CJ2588 <chijioken15@gmail.com>
2024-11-05 14:43:39 -0600

    wk 4 update

---
+++ Back_classproject/server.js
@@ -0,0 +1,16 @@
+import http from "http";
+import app from './app.js'
+
+// const server = http.createServer((req, res) => {
+//   res.end("this is my first response");
+// });
+
+
+
+const port = 3000;
+
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port);
